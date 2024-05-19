4b63b2688d776b8ca78fc624f72bdd998b760405
Abdul Hannan <hannanhaider559@gmail.com>
2024-05-19 08:14:51 +0500

    ll

---
+++ app/head.tsx
@@ -0,0 +1,26 @@
+export default function Head() {
+  return (
+    <>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="My name is Abdul Hannan, Passionate first-year CS student with a can-do attitude. Eager to contribute to a dynamic environment, seeking a competitive role to enhance skills.
+" />
+      <meta name="keywords" content="" />
+      <meta property="og:site_name" content="Abdul Hannan's Portfolio" />
+      <meta property="og:title" content="Portfolio | Abdul Hannan - Full Stack Developer" />
+      <meta property="og:description"
+        content="My name is Abdul Hannan, Passionate first-year CS student with a can-do attitude. Eager to contribute to a dynamic environment, seeking a competitive role to enhance skills.
+" />
+      <meta property="og:url" content="" />
+      <meta property="og:image" content="" />
+      <link rel="canonical" href="" />
+      <meta name="author" content="Abdul Hannan" />
+      <meta name="language" content="English" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="" />
+      <meta name="application-name" content="Portfolio | Abdul Hannan - Full Stack Developer" />
+      <meta name="apple-mobile-web-app-title" content="Portfolio | Abdul Hannan - Full Stack Developer" />
+      <link rel="icon" href="/favicon.ico" />
+      <title>Portfolio | Abdul Hannan - Full Stack Developer</title>
+    </>
+  )
+}
